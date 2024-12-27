da6299acce1f24f1382e394a56384476e1fcc7dc
Luren L. <haoran.sde@gmail.com>
2024-12-26 19:54:15 -0500

    update

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
-export const TESTNET_FULLNODE = "https://aptos.testnet.porto.movementlabs.xyz/v1";
-export const TESTNET_FAUCET = "https://fund.testnet.porto.movementlabs.xyz/";
-export const TESTNET_INDEXER = "https://indexer.testnet.porto.movementnetwork.xyz/v1/graphql";
-export const PACKAGE_ID = '0x2abe2aa6370bfdbe6bc3ce22f7dce1345fbc04f47e25dcd97c320468de0414ca';
-export const INTERNAL_INDEXER = 'http://ec2-34-212-167-187.us-west-2.compute.amazonaws.com:5011/api/TokenPairs';
-export const INTERNAL_RESERVE_INDEXER = "http://ec2-34-212-167-187.us-west-2.compute.amazonaws.com:5011/api/TokenPairPrices";
+// export const TESTNET_FULLNODE = "https://aptos.testnet.porto.movementlabs.xyz/v1";
+// export const TESTNET_FAUCET = "https://fund.testnet.porto.movementlabs.xyz/";
+// export const TESTNET_INDEXER = "https://indexer.testnet.porto.movementnetwork.xyz/v1/graphql";
+// export const PACKAGE_ID = '0x2abe2aa6370bfdbe6bc3ce22f7dce1345fbc04f47e25dcd97c320468de0414ca';
+// export const INTERNAL_INDEXER = 'http://ec2-34-212-167-187.us-west-2.compute.amazonaws.com:5011/api/TokenPairs';
+// export const INTERNAL_RESERVE_INDEXER = "http://ec2-34-212-167-187.us-west-2.compute.amazonaws.com:5011/api/TokenPairPrices";
 
-export const APTOS_TESTNET_FULLNODE = "https://api.testnet.aptoslabs.com/v1";
-export const APTOS_TESTNET_FAUCET = "https://aptos.dev/en/network/faucet";
-export const APTOS_TESTNET_INDEXER = "https://api.testnet.aptoslabs.com/v1/graphql";
-export const APTOS_TESTNET_PACKAGE_ID = "0xe8e1806fb3e05ba81c908e2496a94ac43d64edd900afa90272331eade684f2c7";
-export const APTOS_TESTNET_INTERNAL_INDEXER = "https://aptos-testnet.api.flextech.xyz:443/api/TokenPairs";
-export const APTOS_TESTNET_INTERNAL_RESERVE_INDEXER = "https://aptos-testnet.api.flextech.xyz:443/api/TokenPairPrices";
+export const TESTNET_FULLNODE = "https://api.testnet.aptoslabs.com/v1";
+export const TESTNET_FAUCET = "https://aptos.dev/en/network/faucet";
+export const TESTNET_INDEXER = "https://api.testnet.aptoslabs.com/v1/graphql";
+export const PACKAGE_ID = "0xe8e1806fb3e05ba81c908e2496a94ac43d64edd900afa90272331eade684f2c7";
+export const INTERNAL_INDEXER = "https://aptos-testnet.api.flextech.xyz:443/api/TokenPairs";
+export const INTERNAL_RESERVE_INDEXER = "https://aptos-testnet.api.flextech.xyz:443/api/TokenPairPrices";
